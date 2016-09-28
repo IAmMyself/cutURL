8f0805c059817ad4290a29c160c5e5f02721e237
Mateo Pool <loopoetam@gmail.com>
2016-09-28 14:05:13 +0000

    Finally finished the project

---
+++ server.js
@@ -0,0 +1,96 @@
+var mongo = require('mongodb').MongoClient,
+	express = require('express'),
+	Base62 = require("base62"),
+	nextId = "1",
+	url = 'mongodb://localhost:27017/URLShortener',
+	app = express();
+
+mongo.connect(url, function(err, db) {
+	if (err != undefined) {
+		throw err;
+	}
+    db.collection("links").find().sort({ _id: -1}).toArray(function (err, data) {
+    	if (data[0] != undefined) {
+    		nextId = Base62.encode(Base62.decode(data[0]["_id"]) + 1);
+    	}
+    })
+})
+
+
+app.get("/:id", function (req, res) {
+	var id = req.params.id;
+	mongo.connect(url, function (err, db) {
+		if (err != undefined) {
+			throw err;
+		}
+		db.collection("links").find({
+			"_id": id
+		}).toArray(function (err, data) {
+			if (err != undefined) {
+				throw err;
+			}
+			if (data[0] == undefined) {
+				res.send({
+					"error": "Shortened URL not found in database."
+				});
+				res.end();
+			} else {
+				var theLink = data[0].link;
+				res.redirect(theLink);
+			}
+		})
+	});
+});
+
+app.get("/new/*", function (req, res) {
+	var uRL = req.params[0],
+		isHttp = uRL.slice(0, 11) == "http://www." && uRL.search(".com") != -1,
+		isHttps = uRL.slice(0, 12) == "https://www." && uRL.search(".com") != -1;
+
+	if (isHttp || isHttps) {
+		mongo.connect(url, function (err, db) {
+			if (err != undefined) {
+				throw err;
+			}
+
+			function insert() {
+				db.collection("links").insertOne({
+					"_id": nextId,
+					"link": uRL
+				})
+				var response = {
+					"original_url": uRL,
+					"new_url": "https://url-shortener-fcc-iammyself.c9users.io/" + nextId
+				};
+				res.send(response);
+				res.end()
+				nextId = Base62.encode(Base62.decode(nextId) + 1)
+			}
+
+			db.collection("links").find({
+				"link": uRL
+			}).toArray(function (err, data) {
+				if (err != undefined) {
+					throw err;
+				}
+				if (data[0] != undefined) {
+					var response = {
+						"original_url": uRL,
+						"new_url": "https://url-shortener-fcc-iammyself.c9users.io/" + data[0]["_id"]
+					};
+					res.send(response);
+					res.end();
+				} else {
+					insert();
+				}
+			})
+		})
+	} else {
+		res.send({
+			"error": "Improper URL"
+		});
+		res.end();
+	}
+});
+
+app.listen(process.env.PORT);
